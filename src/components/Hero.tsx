ad150aca26c650698712ccf65442e5382e898dac
Aapush01 <mdshahidafridia31@gmail.com>
2024-11-14 00:50:39 +0530

    [frontend]: Buttons added

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,17 +1,26 @@
-import React from 'react'
-import Timer from './Timer'
+import React from "react";
+import Timer from "./Timer";
 
 const Hero = () => {
   return (
-    <div className='font-mono flex flex-col justify-center items-center'>
-      <div className='flex gap-6 py-10'>
-         <button>Pomodoro</button>
-         <button>Short Break</button>
-         <button>Long Break</button>
+    <div className="font-mono text-lg font-semibold flex flex-col justify-center items-center">
+      <div className="font-mono flex gap-6 py-10">
+        <button className="px-4 py-2 bg-gray-300 text-gray-700 font-semibold rounded-lg shadow-md hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+          Pomodoro
+        </button>
+        <button className="px-4 py-2 bg-gray-300 text-gray-700 font-semibold rounded-lg shadow-md hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+          Short Break
+        </button>
+        <button className="px-4 py-2 bg-gray-300 text-gray-700 font-semibold rounded-lg shadow-md hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+          Long Break
+        </button>
       </div>
-      <div> <Timer/> </div>
+      <div>
+        {" "}
+        <Timer />{" "}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,44 +1,46 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
-const Timer = ({ initialMinutes = 25, initialSeconds = 0}) => {
-    const [minutes, setMinutes] = useState(initialMinutes);
-    const [seconds, setSeconds] = useState(initialSeconds);
-    const [isActive, setIsActive] = useState(false);
+const Timer = ({ initialMinutes = 25, initialSeconds = 0 }) => {
+  const [minutes, setMinutes] = useState(initialMinutes);
+  const [seconds, setSeconds] = useState(initialSeconds);
+  const [isActive, setIsActive] = useState(false);
 
-    // Start or Pause the timer
-    const toggleTimer = () => {
-        setIsActive(!isActive);
-    }
- 
-    //Effect to run the timer
-    useEffect(() => {
-        let timerInterval:any;
-        if(isActive) {
-            timerInterval = setInterval(() => {
-                if(seconds > 0) {
-                    setSeconds(seconds - 1);
-                }else if (seconds === 0 && minutes > 0) {
-                    setMinutes(minutes - 1);
-                    setSeconds(59);
-                } else if(minutes === 0 && seconds === 0) {
-                    setIsActive(false);
-                    alert("Time is up!")
-                }
-            }, 1000);
+  // Start or Pause the timer
+  const toggleTimer = () => {
+    setIsActive(!isActive);
+  };
+
+  //Effect to run the timer
+  useEffect(() => {
+    let timerInterval: any;
+    if (isActive) {
+      timerInterval = setInterval(() => {
+        if (seconds > 0) {
+          setSeconds(seconds - 1);
+        } else if (seconds === 0 && minutes > 0) {
+          setMinutes(minutes - 1);
+          setSeconds(59);
+        } else if (minutes === 0 && seconds === 0) {
+          setIsActive(false);
+          alert("Time is up!");
         }
-        //cleanup interval on component ummount or pause
-        return () => clearInterval(timerInterval);
-    }, [isActive, seconds, minutes])
+      }, 1000);
+    }
+    //cleanup interval on component ummount or pause
+    return () => clearInterval(timerInterval);
+  }, [isActive, seconds, minutes]);
   return (
-    <div className='flex flex-col justify-center items-center py-20'>
-      <div className='font-mono text-9xl font-extrabold '>
-        {String(minutes).padStart(2, "0")}:{String(seconds).padStart(2,"0")}
+    <div className="flex flex-col justify-center items-center py-20">
+      <div className="font-mono text-9xl font-extrabold ">
+        {String(minutes).padStart(2, "0")}:{String(seconds).padStart(2, "0")}
       </div>
-      <div className='font-mono text-3xl font-bold py-28'>
-        <button onClick={toggleTimer}>{isActive? "Pause":"Start"}</button> 
+      <div className="font-mono text-3xl font-bold py-28"> 
+        <button onClick={toggleTimer} className="px-4 py-2 bg-gray-300 text-gray-700 font-semibold rounded-lg shadow-md hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+        {isActive? "Pause":"Start"}
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Timer
+export default Timer;
